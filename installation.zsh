c903778ff8cbcc946919a242337c9e46093944a5
Heber Paim <heber.joan@gmail.com>
2024-10-21 19:07:53 -0300

    modifiying instalation.zsh

--- installation.zsh
+++ installation.zsh
@@ -57,7 +57,7 @@ echo "Configuration updated. Reload your terminal or source your .zshrc to see t
 
 
 # Install neovim
-# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-# sudo rm -rf /opt/nvim
-# sudo tar -C /opt -xzf nvim-linux64.tar.gz
-# echo export PATH="$PATH:/opt/nvim-linux64/bin" >> ~/.zshrc
+ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+ sudo rm -rf /opt/nvim
+ sudo tar -C /opt -xzf nvim-linux64.tar.gz
+ echo export PATH="$PATH:/opt/nvim-linux64/bin" >> ~/.zshrc
